474636f1cf004a1515b626dd96422b36c1541e11
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-12-18 17:19:05 +0100

    Add spanish citizen number validation

--- app/forms/concerns/decidim/account_form_override.rb
+++ app/forms/concerns/decidim/account_form_override.rb
@@ -6,7 +6,13 @@ module Decidim
   module AccountFormOverride
     extend ActiveSupport::Concern
 
+    DNI_LETTERS = "TRWAGMYFPDXBNJZSQVHLCKE"
+    DNI_REGEX = /^(\d{8})([A-Z])$/
+    NIE_REGEX = /^[XYZ]\d{7,8}[A-Z]$/
+
     included do
+      validate :check_document_id
+
       def map_model(model)
         map_model_centers(model)
         map_model_extra_user_fields(model)
@@ -30,6 +36,47 @@ module Decidim
         self.location = extended_data[:location]
         self.profession = extended_data[:profession]
         self.document_id = extended_data[:document_id]
+      end
+
+      def check_document_id
+        return unless document_id?
+
+        case document_type
+        when "DNI"
+          errors.add(:document_id, :invalid) unless valid_dni?(scaped_document_id)
+        when "NIE"
+          errors.add(:document_id, :invalid) unless valid_nie?
+        else
+          errors.add(:document_id, :invalid)
+        end
+      end
+
+      def scaped_document_id
+        @scaped_document_id ||= document_id.upcase.delete("^A-Z0-9")
+      end
+
+      def document_type
+        @document_type ||= if scaped_document_id =~ DNI_REGEX
+                             "DNI"
+                           elsif scaped_document_id =~ NIE_REGEX
+                             "NIE"
+                           end
+      end
+
+      def valid_nie?
+        nie_prefix = case scaped_document_id[0]
+                     when "X"
+                       0
+                     when "Y"
+                       1
+                     else
+                       2
+                     end
+        valid_dni?("#{nie_prefix}#{scaped_document_id[1..]}")
+      end
+
+      def valid_dni?(dni)
+        dni[-1] == DNI_LETTERS[dni.to_i % 23]
       end
     end
   end
